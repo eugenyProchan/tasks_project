83778c850697ff777697c78bbb3837ba81de4669
prochan <prochan@gmail.com>
2020-01-12 22:14:41 +0500

    second_commit

---
+++ tests/func/test_unique_id_1.py
@@ -0,0 +1,23 @@
+"""Test tasks.unique_id()."""
+
+import pytest
+import tasks
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    # Setup : start db
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    yield  # здесь происходит тестирование
+
+    # Teardown : stop db
+    tasks.stop_tasks_db()
+
+
+def test_unique_id():
+    """Вызов unique_id () дважды должен возвращать разные числа."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
---
+++ tests/func/test_unique_id_2.py
@@ -0,0 +1,18 @@
+@pytest.mark.skip(reason='misunderstood the API')
+def test_unique_id_1():
+    """Вызов unique_id () дважды должен возвращать разные числа."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
+
+
+def test_unique_id_2():
+    """unique_id() должен вернуть неиспользуемый id."""
+    ids = []
+    ids.append(tasks.add(Task('one')))
+    ids.append(tasks.add(Task('two')))
+    ids.append(tasks.add(Task('three')))
+    # захват уникального id
+    uid = tasks.unique_id()
+    # убеждаемся, что его нет в списке существующих идентификаторов
+    assert uid not in ids
